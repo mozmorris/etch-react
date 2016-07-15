2d04caf1d15bcbf14118397256c32abea6273e2f
Moz Morris <moz@earthview.co.uk>
2016-07-15 09:23:08 +0100

    React components

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<h1>Hello React!</h1>, document.getElementById('root'));
+const App = props => (
+  <h1>Hello React (stateless component)</h1>
+);
+
+// const App = React.createClass({
+//   render () {
+//     return (
+//       <h1>Hello React (via createClass)</h1>
+//     );
+//   }
+// });
+
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <h1>Hello React (via es6 class)</h1>
+//     );
+//   }
+// }
+
+render(<App />, document.getElementById('root'));
